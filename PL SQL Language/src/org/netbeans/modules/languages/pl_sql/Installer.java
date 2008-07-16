145237a1561d5f6052838bab39f62c4120f3d463
alexandre@sumsoftsolutions.com <alexandre@sumsoftsolutions.com@3a13ac8a-244b-0410-bff2-298120a1e446>
2008-07-16 15:48:51 +0000

    Installeradded

---
+++ PL SQL Language/src/org/netbeans/modules/languages/pl_sql/Installer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.languages.pl_sql;
+
+import org.openide.modules.ModuleInstall;
+import org.openide.util.NbBundle;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    private static final long REQ_MEMORY = 500 * 1024 * 1024; // 500MB
+
+
+    @Override
+    public void validate() throws IllegalStateException {
+        long heapMaxSize = Runtime.getRuntime().maxMemory();
+        if (heapMaxSize < REQ_MEMORY) {
+            String msg = NbBundle.getMessage(Installer.class, "LBL_Memory", heapMaxSize / 1024 / 1024);
+            throw new IllegalStateException(msg);
+        }
+    }
+}
