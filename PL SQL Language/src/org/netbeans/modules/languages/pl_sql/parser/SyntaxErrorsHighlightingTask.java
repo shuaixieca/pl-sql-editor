3fb9608b254c69a56c295f268950d639d294ebb1
alexandre@sumsoftsolutions.com <alexandre@sumsoftsolutions.com@3a13ac8a-244b-0410-bff2-298120a1e446>
2010-04-09 15:59:12 +0000

    Working on parser

--- PL SQL Language/src/org/netbeans/modules/languages/pl_sql/parser/SyntaxErrorsHighlightingTask.java
+++ PL SQL Language/src/org/netbeans/modules/languages/pl_sql/parser/SyntaxErrorsHighlightingTask.java
@@ -3,7 +3,6 @@ package org.netbeans.modules.languages.pl_sql.parser;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.text.Document;
-import javax.swing.text.StyledDocument;
 import org.netbeans.modules.languages.pl_sql.antlr.PL_SQLParser.SyntaxError;
 import org.netbeans.modules.parsing.spi.Parser.Result;
 import org.netbeans.modules.parsing.spi.ParserResultTask;
@@ -13,7 +12,6 @@ import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
 import org.netbeans.spi.editor.hints.HintsController;
 import org.netbeans.spi.editor.hints.Severity;
-import org.openide.text.NbDocument;
 
 public class SyntaxErrorsHighlightingTask extends ParserResultTask {
 
@@ -33,15 +31,15 @@ public class SyntaxErrorsHighlightingTask extends ParserResultTask {
                 if (line <= 0) {
                     continue;
                 }
-                int charPositionInLine = syntaxError.charPositionInLine;
-                int start = NbDocument.findLineOffset((StyledDocument) document, line - 1) + charPositionInLine;
-                int end = start + 1;
+                //int charPositionInLine = syntaxError.charPositionInLine;
+                //int start = NbDocument.findLineOffset((StyledDocument) document, line - 1) + charPositionInLine;
+                //int end = start + 1;
                 ErrorDescription errorDescription = ErrorDescriptionFactory.createErrorDescription(
                         Severity.ERROR,
-                        syntaxError.message,
-                        document,// line);
-                        document.createPosition(start),
-                        document.createPosition(end));
+                        syntaxError.message.replaceAll("COMMA", "','").replaceAll("SEPARATOR", "';'").replaceAll("PARAM_VALUE", "'=>'"),
+                        document,
+                        document.createPosition(syntaxError.start),
+                        document.createPosition(syntaxError.end));
                 errors.add(errorDescription);
             }
             HintsController.setErrors(document, "pl_sql", errors);
