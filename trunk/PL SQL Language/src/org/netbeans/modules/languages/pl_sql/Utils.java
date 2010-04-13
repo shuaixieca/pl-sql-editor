dbbf30d02c6bb8b73d33b3fc579f36fb74ff98a2
alexandre@sumsoftsolutions.com <alexandre@sumsoftsolutions.com@3a13ac8a-244b-0410-bff2-298120a1e446>
2010-04-13 15:40:37 +0000

    Working on code folding

---
+++ trunk/PL SQL Language/src/org/netbeans/modules/languages/pl_sql/Utils.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.languages.pl_sql;
+
+import java.util.ResourceBundle;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author asoumbatov
+ */
+public class Utils {
+
+    public static ResourceBundle getBundle() {
+        return NbBundle.getBundle(Utils.class);
+    }
+}
