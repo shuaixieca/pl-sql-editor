9ecc94269c626322de6104d73c502a7774081aa2
alexandre@sumsoftsolutions.com <alexandre@sumsoftsolutions.com@3a13ac8a-244b-0410-bff2-298120a1e446>
2011-05-09 11:23:02 +0000

    Tagging the 2.2 release

---
+++ tags/release-2.2/PL SQL Editor/src/org/netbeans/modules/languages/pl_sql/editor/oracletree/OObjectType.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.languages.pl_sql.editor.oracletree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.prefs.Preferences;
+import javax.swing.JOptionPane;
+import javax.swing.event.ChangeListener;
+import org.netbeans.modules.languages.pl_sql.editor.Utils;
+import org.netbeans.modules.languages.pl_sql.editor.explorer.nodes.actions.AddCookieInterface;
+import org.netbeans.modules.languages.pl_sql.editor.explorer.nodes.actions.ChangeOAccessCookieInterface;
+import org.netbeans.modules.languages.pl_sql.editor.explorer.nodes.actions.RefreshCookieInterface;
+import org.openide.util.ChangeSupport;
+
+/**
+ *
+ * @author SUMsoft
+ */
+public class OObjectType implements RefreshCookieInterface, ChangeOAccessCookieInterface,
+        AddCookieInterface {
+
+    private ObjectTypes objecttype;
+    private OUser ou;
+    private final ChangeSupport changeSupport = new ChangeSupport(this);
+
+    public OObjectType(ObjectTypes objecttype, OUser ou) {
+        this.objecttype = objecttype;
+        this.ou = ou;
+    }
+
+    public Preferences getPreferencesRoot() {
+        return ou.getPreferencesRoot().node(getObjecttype().toString());
+    }
+
+    public ObjectTypes getObjecttype() {
+        return objecttype;
+    }
+
+    @Override
+    public String toString() {
+        switch (objecttype) {
+            case FUNCTION:
+                return "Functions";
+            case PROCEDURE:
+                return "Procedures";
+            case PACKAGE:
+                return "Packages Spec";
+            case PACKAGE_BODY:
+                return "Packages Body";
+            case TRIGGER:
+                return "Triggers";
+            case TYPE:
+                return "Types Spec";
+            case TYPE_BODY:
+                return "Types Body";
+        }
+        return null;
+    }
+
+    public void addChangeListener(ChangeListener listener) {
+        changeSupport.addChangeListener(listener);
+    }
+
+    public void removeChangeListener(ChangeListener listener) {
+        changeSupport.removeChangeListener(listener);
+    }
+
+    protected void notifyChange() {
+        changeSupport.fireChange();
+    }
+
+    public List<BaseClass> LoadAllObjects() {
+        ObjectTreeSet ots = new ObjectTreeSet(this.objecttype, this.ou.getObjectAccessed(), this.getPreferencesRoot());
+        ots.LoadObjects(ou, this);
+        ArrayList<BaseClass> res = new ArrayList<BaseClass>();
+        for (BaseClass bc : ots) {
+            res.add(bc);
+        }
+        return res;
+    }
+
+    public void Refresh() {
+        this.notifyChange();
+    }
+
+    public void ChangeOAccess(ObjectAccessed oa) {
+        ou.ChangeOAccess(oa);
+    }
+
+    public ObjectAccessed getObjectAccessed() {
+        return ou.getObjectAccessed();
+    }
+
+    public void Add() {
+        String objname = JOptionPane.showInputDialog(null, Utils.getBundle().getString("LBL_TypeNewName"),
+                Utils.getBundle().getString("LBL_CreateObj"), JOptionPane.QUESTION_MESSAGE);
+        if (objname == null || objname.length() == 0) {
+            return;
+        }
+        BaseClass bc = new BaseClass(null, objname.toUpperCase(), objecttype, null, null, null, getPreferencesRoot(), ou, this);
+        bc.setObjectSource(OObjectTypeTemplates.getTemplate(objecttype, objname));
+        bc.edit();
+        if (bc.isLocalFileExists()) {
+            bc.CompileLocalFile();
+            this.Refresh();
+        }
+    }
+}
